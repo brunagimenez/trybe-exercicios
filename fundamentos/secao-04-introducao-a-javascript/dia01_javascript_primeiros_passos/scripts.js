60478deb8a8d593dbf953e91c146623363d7cddb
brunagimenez <brunagimenez97@gmail.com>
2022-10-20 15:32:52 -0300

    exercise 1

---
+++ fundamentos/secao-04-introducao-a-javascript/dia01_javascript_primeiros_passos/scripts.js
@@ -0,0 +1,10 @@
+const myName = "Bruna";
+const birthCity = "São Paulo";
+let birtnYear = 1997;
+console.log(myName," ", birthCity," ",birtnYear);
+
+birtnYear = 2030;
+console.log(birtnYear);
+
+birthCity = "Limeira"; /*Como é uma constante não vai mudar*/
+console.log(birthCity);
