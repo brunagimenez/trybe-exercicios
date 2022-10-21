0bef0163df70de31d4979935ee813ab1f5aa6c97
brunagimenez <brunagimenez97@gmail.com>
2022-10-21 07:35:21 -0300

    exercicio 7

--- fundamentos/secao-04-introducao-a-javascript/dia01_javascript_primeiros_passos/programa6.js
+++ fundamentos/secao-04-introducao-a-javascript/dia01_javascript_primeiros_passos/programa6.js
@@ -1,23 +1,29 @@
-let chessPiece = 'bispo';
+//6°Programa
+//Utilize switch/case para fazer um programa que receba o nome de uma peça de xadrez e
+//retorne os movimentos que ela faz.
+//Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com
+//letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
 
-switch (chessPiece.toLowerCase()){
-    case 'Torre';
+let chessPiece = 'rEi';
+
+switch(chessPiece.toLowerCase()){
+    case 'torre':
         console.log('Torre - move horizontal e vertical.')
         break;
-    case 'Cavalo';
-        console.log('Cavalo - move em L')
+    case 'cavalo':
+        console.log('Cavalo - move em L.')
         break;
-    case 'Bispo';
-        console.log('Bispo - Move em Diagonal')
+    case 'bispo':
+        console.log('Bispo - Move em Diagonal.')
         break;
-    case 'Rei';
+    case 'rei':
         console.log('Rei- Move em diagonal, horizontal e vertical.')
         break;
-    case 'Rainha';
+    case 'rainha':
         console.log('Rainha - Move em diagonal, horizontal e vertical.')
         break;
-    case 'Peão';
-        console.log('Peão - Movem somente para frente')
+    case 'peão':
+        console.log('Peão - Movem somente para frente.')
         break;
     default:
         console.log('Erro, peça inválida!');
