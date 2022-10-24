150b3af7996e3592ce58a8649635fa5d97dd4397
brunagimenez <brunagimenez97@gmail.com>
2022-10-24 18:16:33 -0300

    exercise 1

--- fundamentos/secao-04-introducao-a-javascript/dia03_javascript_logica_De_programacao_e_algoritmos/exercicio1.js
+++ fundamentos/secao-04-introducao-a-javascript/dia03_javascript_logica_De_programacao_e_algoritmos/exercicio1.js
@@ -1,11 +1,6 @@
-let fruits = [3, 4, 10, 1, 12];
-let soma = 0;
+let fatorial = 0;
 
-for(let i = 0; i < fruits.length; i++) {
-    soma += fruits[i];
+for(let i= 0; i>0; i-+1) {
+    fatorial *= i;
 }
-if (soma > 15){
-    console.log(soma);
-} else {
-    console.log('valor menor que 16');
-}
+console.log(fatorial);
