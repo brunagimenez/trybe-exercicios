590529ddf6e668c525fe77901d86f7e14f327c66
brunagimenez <brunagimenez97@gmail.com>
2022-10-26 10:42:35 -0300

    exercise

--- fundamentos/secao-04-introducao-a-javascript/dia04_javascript_objetos_e_funcoes/exercise4.js
+++ fundamentos/secao-04-introducao-a-javascript/dia04_javascript_objetos_e_funcoes/exercise4.js
@@ -26,3 +26,7 @@ leitor.livrosFavoritos.push(
 );
 
 console.log(leitor);
+
+// Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: “Julia tem <quantidade> livros favoritos”, onde “<quantidade>” é um número gerado automaticamente pelo seu código.
+
+console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos ');
