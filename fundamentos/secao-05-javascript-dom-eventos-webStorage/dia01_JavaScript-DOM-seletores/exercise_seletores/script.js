ffd109d1a9c846256cc570438f353dc145569d30
brunagimenez <brunagimenez97@gmail.com>
2022-10-28 16:18:34 -0300

    exercise course

---
+++ fundamentos/secao-05-javascript-dom-eventos-webStorage/dia01_JavaScript-DOM-seletores/exercise_seletores/script.js
@@ -0,0 +1,11 @@
+document.getElementById('header-container').style.backgroundColor = '#00b069';
+
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = '#ff9f84';
+document.querySelectorAll('.emergency-tasks h3')[0].style.backgroundColor = '#a500f3';
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = '#a500f3';
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = '#f9db5e';
+document.querySelectorAll('.no-emergency-tasks h3')[0].style.backgroundColor = '#232525';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = '#232525';
+
+document.getElementById('footer-container').style.backgroundColor ='#003533';
+
