a9ed2fddfca46f7f15fff443447ee60c24c11d7f
brunagimenez <brunagimenez97@gmail.com>
2022-11-01 14:45:07 -0300

    course

--- fundamentos/secao-05-javascript-dom-eventos-webStorage/dia02_JavaScript-Trabalhando_elementos/course.js
+++ fundamentos/secao-05-javascript-dom-eventos-webStorage/dia02_JavaScript-Trabalhando_elementos/course.js
@@ -1,10 +1,19 @@
 // Acesse o elemento elementoOndeVoceEsta.
-// Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
-// Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
-// Acesse o primeiroFilho a partir de pai.
-// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
-// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
-// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
-// Agora acesse o terceiroFilho a partir de pai.
-
 const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+// Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+const elementoPai = elementoOndeVoceEsta.parentElement;
+elementoPai.style.color ='red';
+// Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+const filhoDoFilho = elementoOndeVoceEsta.firstElementChild;
+filhoDoFilho.innerText = 'Algum texto'
+// Acesse o primeiroFilho a partir de pai.
+const primeiroFilho = elementoPai.firstElementChild;
+// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
+const retornarAoPrimeiroFilho = elementoOndeVoceEsta.previousElementSibling;
+// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta.
+const atencao = elementoOndeVoceEsta.nextSibling;
+// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta.
+const terceiroFilho = elementoOndeVoceEsta.nextElementSibling;
+// Agora acesse o terceiroFilho a partir de pai.
+const terceiroFilhoPai = elementoPai.lastElementChild.previousElementSibling;
+
