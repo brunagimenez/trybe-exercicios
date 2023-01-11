720b179d867845633eb12069ede4beecc6ece581
brunagimenez <brunagimenez97@gmail.com>
2023-01-11 13:55:04 -0300

    exercise

--- front-end/dia01_ambiente_de_desenvolvimento/src/main.js
+++ front-end/dia01_ambiente_de_desenvolvimento/src/main.js
@@ -1,5 +1,19 @@
 import validator from 'validator';
 import "./style.css";
 
-console.log(validator.isEmail('foobar.com'));
-console.log('oi');
+const input = document.querySelector('input');
+const select = document.querySelector('select');
+const button = document.querySelector('button');
+const returnPhrase = document.querySelector('p');
+
+button.addEventListener('click', (event) => {
+    event.preventDefault();
+    const campos = {
+        cpf: validator.isTaxID(input.value, 'pt-BR'),
+        data: validator.isDate(input.value, 'DD/MM/YYYY'),
+        email: validator.isEmail(input.value),
+        telefone: validator.isMobilePhone(input.value, 'pt-BR'),
+        maiuscula: validator.isUppercase(input.value),
+      };
+      returnPhrase.innerHTML = `A validação retornou ${campos[select.value]}`;
+});
