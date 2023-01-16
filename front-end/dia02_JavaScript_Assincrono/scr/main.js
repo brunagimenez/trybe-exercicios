057f412c85bdbc37d19c6c18eb9e57594a56b0c7
brunagimenez <brunagimenez97@gmail.com>
2023-01-16 14:09:29 -0300

    exercise

--- front-end/dia02_JavaScript_Assincrono/scr/main.js
+++ front-end/dia02_JavaScript_Assincrono/scr/main.js
@@ -1,2 +1,10 @@
 import Swal from 'sweetalert2';
 import './style.css';
+
+const idAleatorio = Math.floor(Math.random() * (731 - 1) + 1);
+
+ fetch(`https://superheroapi.com/api.php/5914326875293490/${idAleatorio}`)
+   .then((response) => response.json()).then((dados) => console.log(dados));
+
+const imgHero = document.getElementsByTagName('img');
+imgHero.src = "https://www.superherodb.com/pictures2/portraits/10/100/653.jpg";
