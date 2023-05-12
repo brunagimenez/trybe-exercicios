e808f64f3ad1959717113efdbd77219d09c86621
Bruna <brunagimenez97@gmail.com>
2023-05-12 16:21:09 -0300

    add key id in function writeNewMissinData

--- back-end/sessao4-Introdução_ao_desenvolvimento_Web_com_node.js/ProjectMissions/src/index.js
+++ back-end/sessao4-Introdução_ao_desenvolvimento_Web_com_node.js/ProjectMissions/src/index.js
@@ -1,13 +1,17 @@
+const readline = require('readline-sync')
 const { readMissionsData, writeNewMissinData } = require('./utils/fsUtils.js');
 
 const main = async() => {
-    await writeNewMissinData({
-            "id": 100,
-            "name": "Tryber 1",
-            "year": "2019",
-            "country": "Brasil",
-            "destination": "Marte"
-    })
+    const name = readline.question("What is the name of the mission?");
+    const year = readline.questionInt("What is the year of the mission?")
+    const country = readline.question("What is the country of the mission?")
+    const destination = readline.question("What is the destination of the mission?")
+
+    const newMission = {name, year, country, destination};
+    await writeNewMissinData(newMission);
+
+    // const missions = await readMissionsData()
+    // console.log(missions);
 }
 
 main();
--- back-end/sessao4-Introdução_ao_desenvolvimento_Web_com_node.js/ProjectMissions/src/utils/fsUtils.js
+++ back-end/sessao4-Introdução_ao_desenvolvimento_Web_com_node.js/ProjectMissions/src/utils/fsUtils.js
@@ -11,7 +11,7 @@ const readMissionsData = async() => {
       const data = await fs.readFile(path.resolve(__dirname, MISSION_DATA_PATH));
       const missions = JSON.parse(data);
       //JSON.parse para traduzir o Buffer retornado do path
-      return missions;
+    return missions;
   } catch(error) {
     console.error(`Erro na leitura do arquivo: ${error}`);
   }
@@ -21,8 +21,8 @@ const readMissionsData = async() => {
 const writeNewMissinData = async(newMission) => {
     try {
         const oldMissions = await readMissionsData();
-        const allMissions = JSON.stringify([...oldMissions, newMission])
-
+        const allMissions = JSON.stringify([...oldMissions, { id: (oldMissions.length + 1), ...newMission}])
+        // const allMissions = JSON.stringify([...oldMissions, { id: Date.now(), ...newMission}])
         await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH), allMissions)
     } catch (error) {
       console.error(`Erro na leitura do arquivo: ${error}`);
